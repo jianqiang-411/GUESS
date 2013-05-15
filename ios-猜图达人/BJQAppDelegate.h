a688899dca5d2e7c4947bbde150bc074366b4060
mDm <aaronxry@mdm.local>
2013-05-15 14:18:46 +0800

    Initial Commit

---
+++ ios-猜图达人/BJQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BJQAppDelegate.h
+//  ios-猜图达人
+//
+//  Created by mDm on 13-5-15.
+//  Copyright (c) 2013年 mDm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BJQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-猜图达人/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-猜图达人
+//
+//  Created by mDm on 13-5-15.
+//  Copyright (c) 2013年 mDm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BJQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BJQAppDelegate class]));
+    }
+}
